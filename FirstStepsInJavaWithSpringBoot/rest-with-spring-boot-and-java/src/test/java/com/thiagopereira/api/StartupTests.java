50c618ee8ca757156ca6a013c635ce7112accc16
Thiago Pereira <thiagocgp@gmail.com>
2022-09-16 22:42:42 -0300

    First steps in Java with Spring Boot 3

---
+++ FirstStepsInJavaWithSpringBoot/rest-with-spring-boot-and-java/src/test/java/com/thiagopereira/api/StartupTests.java
@@ -0,0 +1,13 @@
+package com.thiagopereira.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
